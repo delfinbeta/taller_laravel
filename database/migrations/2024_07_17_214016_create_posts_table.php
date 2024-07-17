0f2dc13752712019ad6d2a12181dc48439944fa9
Dayan Betancourt <dkbetancourt@gmail.com>
2024-07-17 18:05:21 -0400

    feat :bulb: Migrations

---
+++ database/migrations/2024_07_17_214016_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            // $table->bigInteger('category_id')->unsigned();
+            // $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreignId('category_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+
+            $table->string('title');
+            $table->text('content');
+            $table->integer('points');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
